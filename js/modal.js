023f938994810f982c965253a081313e027e3d83
valeriyanechy <valeriyanechyporuk@gmail.com>
2021-12-03 13:06:19 +0100

    added modal

--- js/modal.js
+++ js/modal.js
@@ -1,33 +1,27 @@
 var btnModal = document.querySelector(".btn__modal");
-
 btnModal.addEventListener('click', function() {
   let modalDiv = document.createElement("div");
   modalDiv.classList.add("modal");
-
   modalDiv.innerHTML = `
     <div class="modal__content">
-        <span class="close">&times;</span>
-        <h1 class="modal__title">Sign In</h1>
-        <p class="modal__text">Please enter information about yourself</p>
-        
-        <form action="">
-            <div class="user-info">
-                <div class="user-value">
-                    <label class="user-title" for="name">Enter your first name and last name:</label>
-                    <input class="user-name" type="text" id="name" placeholder="Name">
-                </div>
-                
-                <div class="row">
-                    <label class="user-title" for="num">Enter your age:</label>
-                    <input type="number" class="user-age" placeholder="Age">
-                </div>
-                <button class="btn btn__ok">OK</button>
-                <button class="btn btn__close">Close</button>
-        </form>
+      <span class="close">&times;</span>
+      <h1 class="modal__title">Sign In</h1>
+      <form action="">
+        <div class="user-info">
+              <div class="user-value">
+                <label class="user-title" for="name">Enter your first name and last name:</label>
+                <input class="user-name" type="text" id="name" placeholder="Name">
+            </div>
+            <div class="user-value-2">
+                <label class="user-title" for="num">Enter your age:</label>
+                <input type="number" class="user-age" placeholder="Age">
+            </div>
+            <button class="btn btn__ok">OK</button>
+            <button class="btn btn__close">Close</button>
+      </form>
     </div>
     `;
   document.body.appendChild(modalDiv);
-
   let span = document.querySelector(".close");
   let btnClose = document.querySelector(".btn__close");
   let btnGetInput = document.querySelector(".btn__ok");
@@ -40,11 +34,10 @@ btnModal.addEventListener('click', function() {
     modalDiv.remove();
   };
 
-btnGetInput.onclick = function() {
+  btnGetInput.onclick = function() {
     let inputName = document.querySelector(".user-name").value;
     let inputAge = document.querySelector(".user-age").value;
-
     console.log(`${inputName}, ${inputAge} age.`);
-}
+  }
 
 });
